d957188b66c84c5ef84f98e54f05269b9fdbfea8
Ousmane! <ousmane.sylla@ynov.com>
2018-10-18 17:52:12 +0200

    creation du menu pour la navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './partials/header/header.component';
+import { FooterComponent } from './partials/footer/footer.component';
+import { UsersComponent } from './pages/users/users/users.component';
+import { UserComponent } from './pages/user/user/user.component';
+import { UpdateComponent } from './pages/update/update.component';
+import { CreateComponent } from './pages/create/create.component';
+import { ProjetsComponent } from './pages/projets/projets.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { EmployeesComponent } from './pages/employees/employees.component';
+import { CustomersComponent } from './pages/customers/customers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    UsersComponent,
+    UserComponent,
+    UpdateComponent,
+    CreateComponent,
+    ProjetsComponent,
+    ProjectsComponent,
+    EmployeesComponent,
+    CustomersComponent
   ],
   imports: [
     BrowserModule
